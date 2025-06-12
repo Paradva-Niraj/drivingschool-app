e72baed0f060a63c41c3e5819307b1b1d609e66c
niraj <nirajparadva2004@gmail.com>
2025-06-12 17:10:55 +0530

    Dashboard Responsive design

--- drivingschool-app/src/pages/Dashboard.jsx
+++ drivingschool-app/src/pages/Dashboard.jsx
@@ -1,7 +1,16 @@
 import '../style/Dashboard.css';
 import Adminnav from '../components/Adminnav';
+import { useState } from 'react';
 
 function Dashboard() {
+
+    const [list,setList] = useState([
+        {title:'Students', count: 2},
+        {title:'Teachers', count: 3},
+        {title:'Parents', count: 5},
+        {title:'Admins', count: 1},
+        {title:'Courses', count: 4},
+    ]);
 
     return (
         <div className='admin'>
@@ -12,8 +21,18 @@ function Dashboard() {
                 <div className='title'>
                     Driving School Management System - admin
                 </div>
-                <div>
-                    
+                <div className='cards'>
+                    {
+                        list.map((item, index) => {
+                            return(
+                                <div className="card" key={index}>
+                                    <img src={`icon${index+1}.png`} alt="info" srcSet="" />
+                                    <span>{item.title}</span>
+                                    <span>{item.count}</span>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
