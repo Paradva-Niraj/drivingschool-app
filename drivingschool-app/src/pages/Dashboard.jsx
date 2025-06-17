2584ccff5ec1acd983550f856d3166fcf0a6084c
niraj <nirajparadva2004@gmail.com>
2025-06-17 16:31:16 +0530

    Handle login waste API call

--- drivingschool-app/src/pages/Dashboard.jsx
+++ drivingschool-app/src/pages/Dashboard.jsx
@@ -3,36 +3,46 @@ import Adminnav from '../components/Adminnav';
 import { useEffect, useState } from 'react';
 import Home from './Home';
 import axios from 'axios';
-import { useNavigate,Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
+import Loading from '../components/Loading.jsx';
 
 function Dashboard() {
-    
+
     const navigate = useNavigate();
-    let [data,setData] = useState(null);
-    // console.log(data.name);
-    
+    let [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
     useEffect(() => {
         const token = localStorage.getItem("token");
-        
-        axios.get(`${import.meta.env.VITE_BASE_URL}/api/admin/dashboard`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        })
-        .then(res => {
-            console.log('Access granted:', res.data);
-            setData(JSON.parse(localStorage.getItem("driving-data")));
-        })
-        .catch(err => {
-            console.error('Access denied:', err.response.data);
+
+        if (localStorage.getItem("token")==null) {
             navigate('/login');
-        });
-    },[]);
+        }
+        else {
+            axios.get(`${import.meta.env.VITE_BASE_URL}/api/admin/dashboard`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+                .then(res => {
+                    console.log('Access granted:', res.data);
+                    setData(JSON.parse(localStorage.getItem("driving-data")));
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.error('Access denied:');
+                    navigate('/login');
+                });
+        }
+    }, []);
+
+    if (loading) { return <Loading /> }
 
     return (
         <div className='admin'>
             <div className='navbar'>
-                <Adminnav/>
+                <Adminnav />
             </div>
             <div className='information'>
                 <div className='title'>
